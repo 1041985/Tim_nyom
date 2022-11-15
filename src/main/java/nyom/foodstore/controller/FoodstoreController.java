d49259b4b003f6751341acc7d3f90059d26159f4
1041985 <alsgk971017@gmail.com>
2022-11-15 12:30:32 +0900

    메인 연결 _ 민하
    (cherry picked from commit f05c9066d13873a8a0f136e4d30b104c87fedfd9)

--- src/main/java/nyom/foodstore/controller/FoodstoreController.java
+++ src/main/java/nyom/foodstore/controller/FoodstoreController.java
@@ -1,5 +1,24 @@
 package nyom.foodstore.controller;
 
-public class FoodstoreController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@AllArgsConstructor
+@Log4j
+public class FoodstoreController {
+	
+	@RequestMapping("main.fs")
+	 public String main(HttpServletRequest request, Model model) {
+			
+	      return "/main";
+	   }
+	
+	
 }
